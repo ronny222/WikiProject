644a4baa06e830adfdfbbd7de334252253d91a84
ronny222 <36170594+ronny222@users.noreply.github.com>
2018-05-15 17:23:41 +0300

    Add files via upload

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -8,22 +8,35 @@ import {of} from 'rxjs/observable/of';
 @Injectable()
 export class ItemService {
 
+  obj: string[] = new Array("");
   constructor() {
   }
 
   getItems(): ListItem[] {
     return myListItems;
   }
+  getItem(id: string): Observable<ListItem> {
 
-  getItem(id: number): Observable<ListItem> {
-
-    return of(myListItems.find(ListItem => ListItem.id === id));
+    return of(myListItems.find(ListItem => ListItem.header === id));
   }
 
   addItem(newItem: ListItem) {
+    console.log(newItem)
     this.getItems().push(newItem);
 
   }
+  getIt(): string[]
+  {
+    let i;
+    for(i=0; i<myListItems.length; i++)
+    {
+      this.obj[i] = myListItems[i].header;
+      console.log(this.obj[i]);
+    }
+    return this.obj;
+  }
+
+
 
 
 }
